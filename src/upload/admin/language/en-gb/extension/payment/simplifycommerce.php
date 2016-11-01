1c3e99e07339ef3ae7a0bdd38c6c6ede690ec73d
hampsterblade <hampsterblade@gmail.com>
2016-10-31 20:27:23 -0400

    Update file structure and controllers for Opencart 2.3

---
+++ src/upload/admin/language/en-gb/extension/payment/simplifycommerce.php
@@ -0,0 +1,57 @@
+<?php
+/*
+@LICENSE@
+ */
+// Heading
+$_['heading_title']      			= 'Simplify Commerce';
+
+// Text 
+$_['text_simplifycommerce']	 		= '<a href="http://www.simplify.com" target="_blank"><img src="view/image/payment/simplifycommerce.jpg" alt="Simplify Commerce" title="Simplify Commerce" style="background-color: #909090; height: 30px"></a>';
+$_['text_test']       				= 'Sandbox';
+$_['text_prod']       				= 'Live';
+$_['text_payment']       			= 'Payment';
+$_['text_success']       			= 'Success: You have modified Simplify Commerce account details!';
+$_['text_off']           			= 'Off';
+$_['text_disabled']           		= 'Disabled';
+$_['text_enabled']           		= 'Enabled';
+$_['text_all_zones']				= 'All Zones';
+$_['text_yes']						= 'Yes';
+$_['text_no']						= 'No';
+$_['text_successful']				= 'Success';
+$_['text_declined']					= 'Declined';
+$_['text_payment_standard']         = 'Standard';
+$_['text_payment_hosted']           = 'Hosted Payments';
+$_['text_edit']						= 'Edit Simplify Commerce';
+
+// Entry
+$_['entry_livesecretkey']     		= 'Live Private API Keys:';
+$_['entry_livepubkey']     			= 'Live Public API Key:';
+$_['entry_testsecretkey']     		= 'Sandbox Private API Key:';
+$_['entry_testpubkey']     			= 'Sandbox Public API Key:';
+$_['entry_title']     				= 'Payment Title:';
+$_['entry_title_help']  			= 'Name of this payment option to be displayed to customer during checkout <br />e.g. Credit Card';
+$_['entry_check_cvc']     			= 'Check CVC';
+$_['entry_webhook_url']     		= 'Webhook URL:';
+$_['entry_webhook_url_help']     	= 'Add this URL in your Simplify Commerce Account at: Your Account->Web Hooks';
+$_['entry_test']					= 'Account Mode:';
+$_['entry_payment_mode']            = 'Payment Mode:';
+$_['entry_button_color']            = 'Hosted Payments Button Color:';
+$_['entry_order_status'] 			= 'Successful Order Status:';
+$_['entry_declined_order_status'] 	= 'Declined Order Status:';
+$_['entry_geo_zone']     			= 'Geo Zone:';
+$_['entry_status']       			= 'Status:';
+$_['entry_sort_order']   			= 'Sort Order:';
+
+// Error
+$_['error_permission']   			= 'Warning: You do not have permission to modify payment Simplify Commerce!';
+$_['error_livesecretkey']     		= 'Valid live private API key required';
+$_['error_livepubkey']     			= 'Valid live public API Key required';
+$_['error_testsecretkey']     		= 'Valid sandbox private API key required';
+$_['error_testpubkey']     			= 'Valid sandbox public API key required';
+$_['error_title']     				= 'Payment Title required';
+$_['error_button_color']            = 'Invalid button color. Should be in hex color format, i.e. #AABBCC';
+
+//Button
+$_['button_save']   				= 'Save';
+$_['button_cancel']   				= 'Cancel';
+?>
